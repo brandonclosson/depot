8d9ab8163da8a00e0ceccf7ba90d8d37e3ade5a2
brandonclosson <bclosson@gmail.com>
2014-07-15 13:56:01 -0400

    Added Carts and LineItems

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,22 @@ class Product < ActiveRecord::Base
 			with: %r{\.(gif|jpg|jpeg|png)\Z}i,
 			message: "must be a url for a valid image file"
 		}
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty? 
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
