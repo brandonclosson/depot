3eb2f7610ab5924626570d324373afed6fad6b99
brandonclosson <bclosson@gmail.com>
2014-07-15 17:21:03 -0400

    Products now show up with quantity once, not multiple times in cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :line_items
-
-  resources :carts
-
   root 'store#index'
-
+  
+  get 'store' => 'store#index'
+  resources :line_items
+  resources :carts
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715174747) do
+ActiveRecord::Schema.define(version: 20140715180645) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
